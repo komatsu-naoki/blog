a9262ad098069e4ff8f78e99bce09a10c85e2ef3
komatsu-naoki <base.ball44.kn.2018@icloud.com>
2019-08-16 16:06:51 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'aa992c12cc8342eca036ee6c3ad355be9eeb5f59109bc7c8fb0abe4b4aa13467f6ba829cd3d912911467ed35fe5444f6fb4ee2e12b59ddf4df8d8a861d793654'
